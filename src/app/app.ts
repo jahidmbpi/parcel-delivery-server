36426c7fbdc931fed10143b249aaa113427088ac
Jahidul Islam <jahid.redmi10@gmail.com>
2025-09-23 15:32:42 +0600

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,7 +7,8 @@ import cookieParser from "cookie-parser";
 const app = Express();
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: " http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
   })
 );
